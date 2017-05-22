ef284e337d69c3c1a188b4382584012f18251358
Bauyrzhanturkpenbayev <mr.turkpenbaev1997@gmail.com>
2017-05-22 19:25:54 +0600

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+	int A;
+	cin >> A;
+	
+	int c = 1, digit;
+	if (A>2){
+		while (A>2){
+			digit = A % 2;
+			A /= 2;
+			if (digit == 1){
+				c++;
+			}
+		}
+	}
+	else if (A == 1){
+		c = 1;
+	}
+	else{
+		c = 0;
+	}
+	cout << c;
+	return 0;
+}
